8d324b6c841a5c754c2d4c22c8911bc953610b61
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-03 11:35:29 -0400

    chore: initial code

---
+++ src/add-objects-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const addObjects = require('.')
+
+describe('add-objects', () => {
+  it('write this test', () => {
+    console.assert(addObjects, 'should export something')
+  })
+})
